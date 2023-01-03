c8c4b01997137bf48095e359179ef7560a5cea6f
RRwW <rw@RWs-Mac-Pro.local>
2023-01-03 10:00:12 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:project/page2.dart';
+import 'dart:math';
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: app(),
+    ),
+  );
+}
